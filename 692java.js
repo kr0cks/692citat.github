2f349719cd8e659040c153a580855c9eeb83fb19
kr0cks <46765165+kr0cks@users.noreply.github.com>
2019-01-22 16:05:52 +0100

    Add files via upload

--- 692java.js
+++ 692java.js
@@ -1,10 +1,14 @@
-function get(){
-var jesper = prompt("Vad är jesper för nåt?");
-if (jesper != "get") {
-alert ("APAPAP Det var visst fel!");
-window.location.href = "https://www.youtube.com/watch?v=c6vwum4EFQw"
-}else {
-  alert ("Det är rätt, han är en get!");
+window.onload = function() {
+    get();
 }
+
+function get() {
+    var jesper = prompt("Vad är jesper för nåt?");
+    if (jesper != "get") {
+        alert("APAPAP Det var visst fel!");
+        window.location.href = "https://www.youtube.com/watch?v=c6vwum4EFQw"
+    } else {
+        alert("Det är rätt, han är en get!");
+    }
 }
 get();
