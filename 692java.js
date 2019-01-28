a3b3a4737e84cdc85bfd4077e2fcc6297074d3d2
kr0cks <46765165+kr0cks@users.noreply.github.com>
2019-01-28 23:10:36 +0100

    Add files via upload

--- 692java.js
+++ 692java.js
@@ -1,10 +1,10 @@
 function get() {
     var jesper = prompt("Vad är jesper för nåt?");
-    if (jesper != "get", "En get", "Get" , "en get") {
-        alert("APAPAP Det var visst fel!");
-        window.location.href = "https://www.youtube.com/watch?v=EaqJgQOVZ9k"
-    } else {
+    if (jesper == "get" || jesper == "En get" || jesper == "Get") {
         alert("Det är rätt, han är en get!");
-    }
+      } else {
+         alert("APAPAP Det var visst fel!");
+         window.location.href = "https://www.youtube.com/watch?v=EaqJgQOVZ9k"
+      }
 }
 get();
